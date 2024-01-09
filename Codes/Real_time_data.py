b03013e5862b3d86f5ee225126f392c26067fa74
Basnayake B.Y.N <131932017+YasiruDEX@users.noreply.github.com>
2024-01-09 23:44:23 +0530

    Update Real_time_data.py
    Added moon data

--- Codes/Real_time_data.py
+++ Codes/Real_time_data.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 Planets = ["Sun", "Mercury", "Venus", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+Moons = ["Ceres", "Pallas", "Juno", "Vesta", "Pluto"]
+
+Bodies = Planets + Moons
 
 # Initialize the WebDriver (you might need to specify the path to your browser driver)
 driver = webdriver.Chrome()  # Change this to your preferred browser driver
@@ -9,12 +12,12 @@ driver = webdriver.Chrome()  # Change this to your preferred browser driver
 # Open the webpage
 driver.get("http://cosinekitty.com/solar_system.html")  # Replace with your website URL
 
-for i in range(0, len(Planets)):
+for i in range(0, len(Bodies)):
     # Find the element by its CSS selector
-    RA = driver.find_element(By.CSS_SELECTOR, "#" + Planets[i] + "_RA").text
-    DEC = driver.find_element(By.CSS_SELECTOR, "#" + Planets[i] + "_DEC").text
+    RA = driver.find_element(By.CSS_SELECTOR, "#" + Bodies[i] + "_RA").text
+    DEC = driver.find_element(By.CSS_SELECTOR, "#" + Bodies[i] + "_DEC").text
 
-    print((Planets[i]+"            ")[0:15], "RA:", RA, "   DEC: " , DEC )
+    print((Bodies[i]+"            ")[0:15], "RA:", RA, "   DEC: " , DEC )
 
 # Close the browser window
 driver.quit()
