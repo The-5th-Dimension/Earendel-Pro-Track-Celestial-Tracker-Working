c0d845bf8d6d2d87aca1f27083c3676089917aa3
Basnayake B.Y.N <YasiruDEX@users.noreply.github.com>
2024-01-11 16:41:58 +0530

    update:LCD integration

--- Codes/Firebase_data_fetch_with_real_time_planet_Locations/Firebase_data_fetch_with_real_time_planet_Locations.ino
+++ Codes/Firebase_data_fetch_with_real_time_planet_Locations/Firebase_data_fetch_with_real_time_planet_Locations.ino
@@ -5,6 +5,9 @@
 #include <ArduinoJson.h>
 #include <ESP8266Firebase.h>
 #include <vector>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 const char* ssid = "UNITY";
 const char* password = "basnayake";
@@ -17,7 +20,23 @@ std::vector<String> planetNames;
 std::vector<String> raValues;
 std::vector<String> decValues;
 
+const int numberOfPlanets = 10; // Assuming 10 planets in our solar system
+
+// Define an array of strings for planet names with moons
+const String planetsWithMoon[numberOfPlanets] = {
+  "Sun",
+  "Moon",
+  "Mercury",
+  "Venus",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune"
+};
+
 String Planet_needed;
+String Star_data;
 
 int findIndex(const std::vector<String>& array, const String &targetValue) {
   auto it = std::find(array.begin(), array.end(), targetValue);
@@ -27,22 +46,46 @@ int findIndex(const std::vector<String>& array, const String &targetValue) {
   return -1;  
 }
 
-
 void connectToWiFi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi ..");
+  lcd.setCursor(0,0);
+  lcd.print("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
-    delay(100);
+    lcd.print('.');
+    delay(1000);
   }
+  lcd.clear();
   Serial.println();
 }
 
-void readFirebaseData() {
+void readFirebaseVariableData() {
   if (WiFi.status() == WL_CONNECTED) {
+    unsigned long startTime = millis();
+
     Planet_needed = firebase.getString("/Planet");
     // Process Firebase data here
+
+    unsigned long endTime = millis();
+    Serial.println("Firebase Reading Time: " + String(endTime - startTime) + " ms");
+    lcd.setCursor(0,0);
+    lcd.print("F:" + String(endTime - startTime) + "ms");
+  }
+}
+
+void readFirebaseStarData() {
+  if (WiFi.status() == WL_CONNECTED) {
+    unsigned long startTime = millis();
+
+    Star_data = firebase.getString("/" + Planet_needed);
+    // Process Firebase data here
+
+    unsigned long endTime = millis();
+    lcd.setCursor(10,0);
+    lcd.print("S:" + String(endTime - startTime) + "ms");
+    Serial.println("Firebase Star Reading Time: " + String(endTime - startTime) + " ms");
   }
 }
 
@@ -51,7 +94,9 @@ void fetchDataFromAPI() {
     std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
     client->setInsecure();
     HTTPClient https;
-    
+
+    unsigned long startTime = millis();
+
     if (https.begin(*client, "https://api.visibleplanets.dev/v3?latitude=7.4818&longitude=80.3609&showCoords=true&aboveHorizon=false")) {
       int httpCode = https.GET();
       if (httpCode > 0) {
@@ -87,6 +132,12 @@ void fetchDataFromAPI() {
       } else {
         Serial.printf("[https] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
       }
+
+      unsigned long endTime = millis();
+      Serial.println("HTTP Fetching Time: " + String(endTime - startTime) + " ms");
+      lcd.setCursor(10,0);
+      lcd.print("H: " + String(endTime - startTime) + "ms");
+
       https.end();
     } else {
       Serial.printf("[https] Unable to connect\n");
@@ -95,6 +146,11 @@ void fetchDataFromAPI() {
 }
 
 void setup() {
+
+  lcd.init();                      // initialize the lcd 
+  // Print a message to the LCD.
+  lcd.backlight();
+
   Serial.begin(115200);
   Serial.println();
   Serial.println();
@@ -104,17 +160,52 @@ void setup() {
 }
 
 void loop() {
-  fetchDataFromAPI();
-  readFirebaseData();
-  
-  int index = findIndex(planetNames, Planet_needed);
-  if (index != -1) {
-    Serial.println(Planet_needed + " : RA: " + raValues[index] + " : DEC:" + decValues[index]);
-  } else {
-    Serial.println("Planet not found!");
-  }
-  Serial.println();
-  Serial.println("Waiting 1S before the next round...");
-  delay(1000);
-}
+  readFirebaseVariableData();
 
+  // Check if the desired planet is in the array
+  bool planetFound = false;
+  for (int i = 0; i < numberOfPlanets; i++) {
+    if (Planet_needed.equals(planetsWithMoon[i])) {
+      planetFound = true;
+      break; // Exit the loop once the planet is found
+    }
+  }
+
+  // If the planet is found, fetch data from the API; otherwise, delay and then read from Firebase
+  if (planetFound) {
+    fetchDataFromAPI();
+
+    int index = findIndex(planetNames, Planet_needed);
+    if (index != -1) {
+      Serial.println(Planet_needed + " : RA: " + raValues[index] + " : DEC:" + decValues[index]);
+      lcd.setCursor(0,1);
+      lcd.print("              ");
+      lcd.setCursor(0,1);
+      lcd.print(Planet_needed + ":");
+      lcd.setCursor(0,2);
+      lcd.print( "RA: " + String(raValues[index]) );
+      lcd.setCursor(0,3);
+      lcd.print( "DEC: " + String(decValues[index]) );
+    } else {
+      Serial.println("Planet not found!");
+    }
+
+  } else {
+    readFirebaseStarData();
+
+    Serial.println(Planet_needed + ": " + Star_data);
+    lcd.setCursor(0,1);
+    lcd.print("              ");
+    lcd.setCursor(0,1);
+    lcd.print(Planet_needed + ":");
+    lcd.setCursor(0,2);
+    lcd.print(Star_data.substring(0, 10));
+    lcd.setCursor(0,3);
+    lcd.print(Star_data.substring(11));
+    
+  }
+  
+  Serial.println();
+  // Serial.println("Waiting 1S before the next round...");
+  // delay(1000);
+}
