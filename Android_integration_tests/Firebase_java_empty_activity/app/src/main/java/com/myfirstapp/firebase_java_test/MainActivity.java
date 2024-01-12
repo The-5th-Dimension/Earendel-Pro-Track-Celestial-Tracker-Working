e86757113651152c567109ce4b4b77e3fa156ae4
Basnayake B.Y.N <131932017+YasiruDEX@users.noreply.github.com>
2024-01-12 12:17:19 +0530

    Added: Android integration testing

---
+++ Android_integration_tests/Firebase_java_empty_activity/app/src/main/java/com/myfirstapp/firebase_java_test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.myfirstapp.firebase_java_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
