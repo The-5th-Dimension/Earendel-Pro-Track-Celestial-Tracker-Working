e4238929533d8abac10bcde7b6777309bfeb4659
Basnayake B.Y.N <YasiruDEX@users.noreply.github.com>
2024-01-13 11:05:33 +0530

    Update API_and_Firebase_data_fetch_with_actuators.ino

--- Codes/API_and_Firebase_data_fetch_with_actuators/API_and_Firebase_data_fetch_with_actuators.ino
+++ Codes/API_and_Firebase_data_fetch_with_actuators/API_and_Firebase_data_fetch_with_actuators.ino
@@ -5,92 +5,107 @@
 #include "ServoManager.h"
 #include "LCDManager.h"
 
-ServoManager Zaxis;
+ServoManager zAxisServo;
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 const char* ssid = "UNITY";
 const char* password = "basnayake";
-#define REFERENCE_URL "earendeldata-default-rtdb.asia-southeast1.firebasedatabase.app"
+const char* referenceUrl = "earendeldata-default-rtdb.asia-southeast1.firebasedatabase.app";
+const char* httpUrl = "https://api.visibleplanets.dev/v3?latitude=7.4818&longitude=80.3609&showCoords=true&aboveHorizon=false";
 
-FirebaseManager firebase(REFERENCE_URL);
-
-WiFiManager Wireless(ssid, password, lcd);
-
-HTTPManager HTTPmanager;
+FirebaseManager firebase(referenceUrl);
+WiFiManager wifiManager(ssid, password, lcd);
+HTTPManager httpManager;
 
 const int MAX_PLANETS = 10;
+const int numberOfPlanets = 10;
+
 String planetNames[MAX_PLANETS];
 String raValues[MAX_PLANETS];
 String decValues[MAX_PLANETS];
 
-const int numberOfPlanets = 10;
 const String planetsWithMoon[numberOfPlanets] = {
   "Sun", "Moon", "Mercury", "Venus", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"
 };
 
-String Planet_needed;
-String Star_data;
-String prev_planet;
+String planetNeeded;
+String starData;
+String prevPlanet;
 
+// Function declarations
 int findIndex(const String array[], int size, const String &targetValue);
+void initialize();
+void handlePlanetData();
+void handleStarData();
 
 void setup() {
-  Zaxis.attach(D3);
+  zAxisServo.attach(D3);
   lcd.init();
   lcd.backlight();
   Serial.begin(115200);
   Serial.println();
   Serial.println();
   Serial.println();
-  Wireless.connect();
+  initialize();
 }
 
 void loop() {
-  Planet_needed = firebase.getString("Planet");
-  bool planetFound = false;
-  for (int i = 0; i < numberOfPlanets; i++) {
-    if (Planet_needed.equals(planetsWithMoon[i])) {
-      planetFound = true;
-      break;
-    }
+  planetNeeded = firebase.getString("Planet");
+  
+  bool planetFound = findIndex(planetsWithMoon, numberOfPlanets, planetNeeded) != -1;
+  
+  if (planetFound) {
+    handlePlanetData();
+  } else {
+    handleStarData();
   }
 
-  if (planetFound) {
-    HTTPmanager.fetchData("https://api.visibleplanets.dev/v3?latitude=7.4818&longitude=80.3609&showCoords=true&aboveHorizon=false", planetNames, raValues, decValues);
-    int index = findIndex(planetNames, MAX_PLANETS, Planet_needed);
-    if (index != -1) {
-      int degreeIndex = raValues[index].indexOf('h');
-      Serial.println(Planet_needed + " : RA: " + raValues[index] + " : DEC:" + decValues[index]);
-      Zaxis.write(atoi(raValues[index].substring(0, degreeIndex).c_str()) * 9);
-      lcd.setCursor(0, 1);
-      lcd.print("              ");
-      lcd.setCursor(0, 1);
-      lcd.print(Planet_needed + ":");
-      lcd.setCursor(0, 2);
-      lcd.print("RA: " + String(raValues[index]));
-      lcd.setCursor(0, 3);
-      lcd.print("DEC: " + String(decValues[index]));
-    } else {
-      Serial.println("Planet not found!");
-    }
-  } else {
-    if (Planet_needed != prev_planet) {
-      Star_data = firebase.getString(Planet_needed);
-    }
+  prevPlanet = planetNeeded;
+  Serial.println();
+}
 
-    Serial.println(Planet_needed + ": " + Star_data);
+// Function definitions
+void initialize() {
+  wifiManager.connect();
+}
+
+void handlePlanetData() {
+  httpManager.fetchData(httpUrl, planetNames, raValues, decValues);
+
+  int index = findIndex(planetNames, MAX_PLANETS, planetNeeded);
+
+  if (index != -1) {
+    int degreeIndex = raValues[index].indexOf('h');
+    Serial.println(planetNeeded + " : RA: " + raValues[index] + " : DEC:" + decValues[index]);
+    zAxisServo.write(atoi(raValues[index].substring(0, degreeIndex).c_str()) * 9);
+
     lcd.setCursor(0, 1);
     lcd.print("              ");
     lcd.setCursor(0, 1);
-    lcd.print(Planet_needed + ":");
+    lcd.print(planetNeeded + ":");
     lcd.setCursor(0, 2);
-    lcd.print(Star_data.substring(0, 10));
+    lcd.print("RA: " + String(raValues[index]));
     lcd.setCursor(0, 3);
-    lcd.print(Star_data.substring(11, 24));
+    lcd.print("DEC: " + String(decValues[index]));
+  } else {
+    Serial.println("Planet not found!");
+  }
+}
+
+void handleStarData() {
+  if (planetNeeded != prevPlanet) {
+    starData = firebase.getString(planetNeeded);
   }
 
-  prev_planet = Planet_needed;
-  Serial.println();
+  Serial.println(planetNeeded + ": " + starData);
+  lcd.setCursor(0, 1);
+  lcd.print("              ");
+  lcd.setCursor(0, 1);
+  lcd.print(planetNeeded + ":");
+  lcd.setCursor(0, 2);
+  lcd.print(starData.substring(0, 10));
+  lcd.setCursor(0, 3);
+  lcd.print(starData.substring(11, 24));
 }
 
 int findIndex(const String array[], int size, const String &targetValue) {
