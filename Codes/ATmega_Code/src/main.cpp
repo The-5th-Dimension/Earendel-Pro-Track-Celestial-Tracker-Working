3f9c1042c206e07de168721f116e47295f61be8f
Linuka Ratnayake <79963204+linukaratnayake@users.noreply.github.com>
2024-02-29 09:36:49 +0530

    Update project plan as comments on main.cpp for easy coding

--- Codes/ATmega_Code/src/main.cpp
+++ Codes/ATmega_Code/src/main.cpp
@@ -1,7 +1,19 @@
 #include <Arduino.h>
 
-// put function declarations here:
+/*
 
+PROJECT PLAN
+
+1. Use external interrrupts to trigger a UART transfer from the ESP32.
+2. Get serial data about GPS location, UTC time, and RA-DEC coordinates.
+3. Use the already built library to convert RA-DEC to ALT-AZ.
+4. Turn the stepper motors to the required stellar object.
+5. Keep focusing until another interrupt gives another object.
+(Time updates can also come from an interrupt.)
+
+*/
+
+// put function declarations here:
 
 void setup() {
   // put your setup code here, to run once:
