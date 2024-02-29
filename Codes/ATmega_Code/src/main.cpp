478c88c75950245d2b0523919cd40ea55542b2cd
Linuka Ratnayake <79963204+linukaratnayake@users.noreply.github.com>
2024-02-29 19:43:46 +0530

    Add timer interrupts to update local time

--- Codes/ATmega_Code/src/main.cpp
+++ Codes/ATmega_Code/src/main.cpp
@@ -15,7 +15,7 @@ PROJECT PLAN
 #include <StandardCplusplus.h>
 #include "CoordinateConverter.h"
 
-#define SerialINT 2
+#define Serial_INT 2
 
 // Variables to be updated based on the Serial data from the ESP32.
 volatile int year = 2000, month = 1, day = 1, hour = 12, minute = 0;
@@ -27,11 +27,14 @@ double ra = 0, dec = 0;
 double altitude = 0, azimuth = 0;
 
 void handleSerial();
+void autoUpdateTime();
 
 void setup()
 {
-  pinMode(SerialINT, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(SerialINT), handleSerial, RISING);
+  pinMode(Serial_INT, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(Serial_INT), handleSerial, RISING);
+
+  autoUpdateTime();
 }
 
 void loop()
@@ -42,4 +45,41 @@ void loop()
 void handleSerial()
 {
   // The function definition depends on how the ESP32 encodds data.
+}
+
+void autoUpdateTime()
+{
+  // The following are internal registers in ATmega328.
+  // Internal interrupts are used to precissely keep track of time.
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 3036;
+  TCCR1B |= (1 << CS12);
+  TIMSK1 |= (1 << TOIE1);
+}
+
+ISR(TIMER1_OVF_vect)
+{
+  second += 1;
+  if (second == 60)
+  {
+    second = 0;
+    minute += 1;
+  }
+  if (minute == 60)
+  {
+    minute = 0;
+    hour += 1;
+  }
+  if (hour == 24)
+  {
+    hour = 0;
+    day += 1;
+  }
+  // This should be extended to work well with month and year boundaries.
+  // e.g.: Update from 31st March to 1st April.
+  // However, let's assume this is enough for demonstration.
+
+  // Setting back to 3036, otherwise it starts back from 0.
+  TCNT1 = 3036;
 }
