3ab2c0124be96b473575dddb8b48c1a94f85fb2a
Linuka Ratnayake <79963204+linukaratnayake@users.noreply.github.com>
2024-03-22 18:15:49 +0530

    Update value for Steps per Revolution

--- Codes/ATmega_Code/include/PseudoMagnetometer.h
+++ Codes/ATmega_Code/include/PseudoMagnetometer.h
@@ -1,7 +1,7 @@
 #ifndef PSEUDO_MAGNETOMETER_H
 #define PSEUDO_MAGNETOMETER_H
 
-#define STEPS_PER_REVOLUTION 1000   // Should update to the correct value.
+#define STEPS_PER_REVOLUTION 3200   // Should update to the correct value.
 
 unsigned long int angleToSteps(double angle);
 double stepsToAngle(unsigned long int steps);
